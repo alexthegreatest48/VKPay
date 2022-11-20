7fcd2b5833ac6d1d62faa535772fffcbfd5bbff4
alexthegreatest48 <as.anisimov@inbox.ru>
2022-11-20 19:12:23 +0300

    Fake error

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -6,5 +6,12 @@ internal class MainKtTest {
 
     @Test
     fun commission() {
+        val typeCard = 2 // 1 - VKpay 2 - Visa Мир 3 - Master Maestro
+        val sum = 30
+        var sumOverallVK = 0
+        var sumOverallCard = 0 // Общая сумма переводов для всех карт
+
+        val result = commission(typeCard, sumOverallCard, sum)
+        assertEquals(30, result)
     }
 }
